fbaf83b6f1eeec9db759cc9ecb1bbd88f2307f5c
Sunny Arokia Swamy Bellary <sunnysonu@Sunnys-MacBook-Pro.local>
2024-01-30 17:21:27 -0500

    removed db

--- application.py
+++ application.py
@@ -21,11 +21,11 @@ bcrypt = Bcrypt(application)
 login_manager = LoginManager()
 login_manager.init_app(application)
 login_manager.login_view = 'login'
-
+'''
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
+'''
 class Userlocal:
     def __init__(self, un,pw):
         self.un = un
